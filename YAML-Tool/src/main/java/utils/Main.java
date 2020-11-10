af08ca4087cab861a1abb97e0764df4229b38305
Optimus <Optimus@DESKTOP-8KKQ6H9>
2020-11-10 17:39:29 +0100

    yamltool

--- YAML-Tool/src/main/java/utils/Main.java
+++ YAML-Tool/src/main/java/utils/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args)
 	{
 		YAMLImplementation yaml = new YAMLImplementation();
-		yaml.openFile(new File(""));
-	
+		yaml.openDir("podaci");
+		yaml.save(yaml.entities, "export.yaml");
 	}
 
 }
--- YAML-Tool/src/main/java/utils/YAMLImplementation.java
+++ YAML-Tool/src/main/java/utils/YAMLImplementation.java
@@ -1,41 +1,73 @@
 package utils;
 
-import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.InputStream;
-import java.util.Map;
-import java.util.Map.Entry;
+import java.io.StringWriter;
+import java.util.List;
 
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
 import models.Entity;
 
-public class YAMLImplementation extends API{
-
+public class YAMLImplementation extends API
+{
 	@Override
-	public void openFile(File file) {
+	public void openFile(File file) 
+	{
 		Yaml yaml = new Yaml(new Constructor(Entity.class));
-		InputStream inputStream = this.getClass().getResourceAsStream("podaci.yaml");
-		//Entity e = yaml.load(inputStream);
-		//System.out.println(e.toString());
 		
-	    for (Object e : yaml.loadAll(inputStream)) {
-			System.out.println("Ime i id: ");
-			System.out.println(((Entity) e).getId() + " " + ((Entity) e).getName() + "\n");
-			System.out.println("Propertiji:");
-			for (Map.Entry<String,Object> entry : ((Entity) e).getProperties().entrySet()) {
-	            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue()); 
-			}
-			System.out.println();
-			System.out.println("Entitiji");
-			for (Map.Entry<String, Entity> entry : ((Entity) e).getEntities().entrySet()) {
-	            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue()); 
-			}
-			System.out.println("-----------------");
-	    }
+		try 
+		{
+			InputStream inputStream = new FileInputStream(file);
+			
+		    for (Object e : yaml.loadAll(inputStream)) 
+		    {
+				entities.add((Entity) e);
+		    }
+		} 
+		catch (FileNotFoundException e1) 
+		{
+			e1.printStackTrace();
+		}
 		
 
 	}
 
+	@Override
+	public void enterEntity(String arg0)
+	{
+		
+	}
+
+	@Override
+	public void save(List<Entity> data, String fileName) 
+	{
+		try
+		{
+			File file = new File("export/" + fileName);
+			
+			file.createNewFile();
+			
+			FileWriter fw = new FileWriter("export/" + fileName);
+			
+			for(Entity entity: data)
+			{
+			    Yaml yaml = new Yaml();
+			    StringWriter object = new StringWriter();
+			    yaml.dump(entity, object);  
+			    fw.write(object.toString());
+			    fw.write("---\n");
+			}
+			fw.flush();
+			fw.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
