7963a296c7b968468c04be45dc18e0ef53dd78a2
DESKTOP-L6O7F6S\bosko <boskovicnikola99@gmail.com>
2020-11-11 15:14:29 +0100

    a

--- YAML-Tool/src/main/java/utils/YAMLImplementation.java
+++ YAML-Tool/src/main/java/utils/YAMLImplementation.java
@@ -8,18 +8,25 @@ import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.List;
 
+import org.yaml.snakeyaml.DumperOptions.FlowStyle;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.nodes.Tag;
 
 import models.Entity;
 
 public class YAMLImplementation extends API
 {
+	static {
+		ToolManager.registerManager(new YAMLImplementation(), ".yaml");
+	}
+	public YAMLImplementation() {
+		super();
+	}
 	@Override
 	public void openFile(File file) 
-	{
+	{	
 		Yaml yaml = new Yaml(new Constructor(Entity.class));
-		
 		try 
 		{
 			InputStream inputStream = new FileInputStream(file);
@@ -44,30 +51,38 @@ public class YAMLImplementation extends API
 	}
 
 	@Override
-	public void save(List<Entity> data, String fileName) 
-	{
-		try
-		{
-			File file = new File("export/" + fileName);
-			
-			file.createNewFile();
-			
-			FileWriter fw = new FileWriter("export/" + fileName);
-			
-			for(Entity entity: data)
-			{
-			    Yaml yaml = new Yaml();
-			    StringWriter object = new StringWriter();
-			    yaml.dump(entity, object);  
-			    fw.write(object.toString());
-			    fw.write("---\n");
-			}
-			fw.flush();
-			fw.close();
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
+    public void save(List<Entity> data, String fileName) 
+    {
+        try
+        {
+            File file = new File("export/" + fileName);
+
+            file.createNewFile();
+
+            FileWriter fw = new FileWriter("export/" + fileName);
+
+            for(Entity entity: data)
+            {
+                Yaml yaml = new Yaml();
+                String text = yaml.dumpAs(entity, Tag.MAP, FlowStyle.BLOCK);
+                fw.write(text + "---\n");
+            }
+            fw.flush();
+            fw.close();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+	@Override
+	public void brisiKljucVrednost(String arg0, String arg1, String arg2, String arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void brisiNaOsnovuIDa(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 }
